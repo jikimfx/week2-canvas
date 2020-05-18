0e347aff3941d4ce1fca579a2f32c2f3f896b630
jikimfx <jikimfx@gmail.com>
2020-05-18 09:40:55 +0700

    Monster Catch

---
+++ game.js
@@ -0,0 +1,210 @@
+/*
+  Code modified from:
+  http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
+  using graphics purchased from vectorstock.com
+*/
+
+/* Initialization.
+Here, we create and add our "canvas" to the page.
+We also load all of our images. 
+*/
+
+
+let canvas;
+let ctx;
+
+canvas = document.createElement("canvas");
+ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+let audio = new Audio();
+audio.src = "monstercatch.mp3"
+
+let bgReady, heroReady, monsterReady;
+let bgImage, heroImage, monsterImage;
+
+const applicationState = {
+    isGameOver: true,
+    currentUser: "",
+    highScore: {
+        currentUser: "",
+        score: 0,
+    },
+};
+
+let playerName = document.getElementById("playerName");
+let currentScore = 0;
+let startTime = Date.now();
+const SECONDS_PER_ROUND = 15;
+let elapsedTime = 0;
+
+function loadImages() {
+    bgImage = new Image();
+    bgImage.onload = function () {
+        // show the background image
+        bgReady = true;
+    };
+    bgImage.src = "images/background.png";
+    heroImage = new Image();
+    heroImage.onload = function () {
+        // show the hero image
+        heroReady = true;
+    };
+    heroImage.src = "images/hero.png";
+
+    monsterImage = new Image();
+    monsterImage.onload = function () {
+        // show the monster image
+        monsterReady = true;
+    };
+    monsterImage.src = "images/monster.png";
+}
+
+/** 
+ * Setting up our characters.
+ * 
+ * Note that heroX represents the X position of our hero.
+ * heroY represents the Y position.
+ * We'll need these values to know where to "draw" the hero.
+ * 
+ * The same applies to the monster.
+ */
+
+let heroX = canvas.width / 2;
+let heroY = canvas.height / 2;
+
+let monsterX = 100;
+let monsterY = 100;
+
+function getPlayerName() {
+    applicationState.currentUser = playerName.value;
+    applicationState.isGameOver = false;
+    startTime = Date.now();
+    document.getElementById("displayPlayerName").innerHTML = `Current Player: ${applicationState.currentUser}`;
+    document.getElementById("displayPlayerName").classList.add('playerActive');
+    $('.active').removeClass("hide").addClass('hide').removeClass("active");
+    $('.playerActive').removeClass("playerHide").addClass('playerActive');
+}
+
+/** 
+ * Keyboard Listeners
+ * You can safely ignore this part, for now. 
+ * 
+ * This is just to let JavaScript know when the user has pressed a key.
+*/
+let keysDown = {};
+function setupKeyboardListeners() {
+    // Check for keys pressed where key represents the keycode captured
+    // For now, do not worry too much about what's happening here. 
+    addEventListener("keydown", function (key) {
+        keysDown[key.keyCode] = true;
+    }, false);
+
+    addEventListener("keyup", function (key) {
+        delete keysDown[key.keyCode];
+    }, false);
+}
+
+
+/**
+ *  Update game objects - change player position based on key pressed
+ *  and check to see if the monster has been caught!
+ *  
+ *  If you change the value of 5, the player will move at a different rate.
+ */
+let update = function () {
+    if (applicationState.isGameOver == true) {
+        return applicationState.isGameOver;
+    }
+    // Update the time.
+    elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+
+    if (SECONDS_PER_ROUND - elapsedTime == 0) { applicationState.isGameOver = true; }
+    if (applicationState.highScore.score < currentScore) {
+        applicationState.highScore.score = currentScore;
+        document.getElementById("highScoreDisplay").innerHTML = `High Score: ${applicationState.highScore.score}`;
+    }
+
+    if (38 in keysDown) { // Player is holding up key
+        heroY -= 5;
+    }
+    if (40 in keysDown) { // Player is holding down key
+        heroY += 5;
+    }
+    if (37 in keysDown) { // Player is holding left key
+        heroX -= 5;
+    }
+    if (39 in keysDown) { // Player is holding right key
+        heroX += 5;
+    }
+
+    // Check if player and monster collided. Our images
+    // are about 32 pixels big.
+    if (
+        heroX <= (monsterX + 32)
+        && monsterX <= (heroX + 32)
+        && heroY <= (monsterY + 32)
+        && monsterY <= (heroY + 32)
+    ) {
+        // Pick a new location for the monster.
+        // Note: Change this to place the monster at a new, random location.
+        monsterX = Math.floor(Math.random() * (canvas.width - 32))
+        monsterY = Math.floor(Math.random() * (canvas.height - 32))
+        audio.play();
+        currentScore++;
+    }
+};
+
+/**
+ * This function, render, runs as often as possible.
+ */
+var render = function () {
+    if (bgReady) {
+        ctx.drawImage(bgImage, 0, 0);
+    }
+    if (heroReady) {
+        ctx.drawImage(heroImage, heroX, heroY);
+    }
+    if (monsterReady) {
+        ctx.drawImage(monsterImage, monsterX, monsterY);
+    }
+    ctx.font = "15px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(`Seconds Remaining: ${SECONDS_PER_ROUND - elapsedTime}`, canvas.width / 2, 70);
+    ctx.fillText(`Current Score: ${currentScore}`, canvas.width / 2, 50);
+};
+
+function resetGame() {
+    applicationState.isGameOver = true;
+    elapsedTime = 0;
+    currentScore = 0;
+    playerName.innerHTML = ``;
+    $('.hide').removeClass("active").addClass('active').removeClass("hide");
+    $('.playerActive').removeClass("playerActive").addClass('playerHide');
+}
+
+/**
+ * The main game loop. Most every game will have two distinct parts:
+ * update (updates the state of the game, in this case our hero and monster)
+ * render (based on the state of our game, draw the right things)
+ */
+var main = function () {
+    update();
+    render();
+    // Request to do this again ASAP. This is a special method
+    // for web browsers. 
+    requestAnimationFrame(main);
+};
+
+// Cross-browser support for requestAnimationFrame.
+// Safely ignore this line. It's mostly here for people with old web browsers.
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+// Let's play this game!
+loadImages();
+setupKeyboardListeners();
+main();
